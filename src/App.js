152053d41af115bf3f34e68f1cff83b268b08d35
flipott <ottphillipa@gmail.com>
2023-02-02 20:41:30 -0500

    Render basic index page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import IndexLayout from "./components/index/IndexLayout";
+import Login from "./components/index/Login";
+import Register from "./components/index/Register";
+import About from "./components/index/About";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<IndexLayout component={<Login />} />} />
+        <Route path="/register" element={<IndexLayout component={<Register />} />} />
+        <Route path="/about" element={<IndexLayout component={<About />} />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
