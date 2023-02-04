776c6a6aeb565bf0e19d41dda27df7556ef90d3f
flipott <ottphillipa@gmail.com>
2023-02-03 19:54:50 -0500

    Animate text loop

--- src/components/index/IndexLayout.js
+++ src/components/index/IndexLayout.js
@@ -1,20 +1,37 @@
 import React from "react";
 import IndexFooter from "./IndexFooter";
-
-const animatedText = [
-    `<p class="animated-text">Socializing made <span class="animated-orange">simple.</span></p>`,
-    `<p class="animated-text"><span class="animated-orange">Connect</span> with friends.</p>`,
-    `<p class="animated-text"><span class="animated-orange">Share</span> your thoughts.</p>`,
-]
+import { useState, useEffect } from "react";
 
 export default function IndexLayout(props) {
+
+    const animatedText = [
+        `<p class="animated-text">Socializing made <span class="animated-orange">simple.</span></p>`,
+        `<p class="animated-text"><span class="animated-orange">Connect</span> with friends.</p>`,
+        `<p class="animated-text"><span class="animated-orange">Share</span> your thoughts.</p>`,
+    ]
+
+    const [text, setText] = React.useState(animatedText[0]);
+    const [textIndex, setTextIndex] = React.useState(1)
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setText(animatedText[textIndex]);
+            if (textIndex + 1 >= animatedText.length) {
+                setTextIndex(0);
+            } else {
+                setTextIndex((prev) => prev + 1);
+            }
+        }, 5000);
+        return () => clearInterval(interval);
+    }, [textIndex])
+
     return (
         <div className="page-wrap">
             <div className="index-wrap">
                 <div className="index-main">
                     <div class="hero-left">
                         <img src="/images/logo-light.svg" />
-                        <div dangerouslySetInnerHTML={{ __html: animatedText[2] }} />
+                        <div dangerouslySetInnerHTML={{ __html: text }} />
                     </div>
                     {props.component}
                 </div>
