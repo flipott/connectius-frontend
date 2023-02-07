90771121819af7baf6b9d8f36ecd94f05c0e60a5
flipott <ottphillipa@gmail.com>
2023-02-07 04:47:50 -0500

    Create main stylesheet

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './styles/app.css';
+
 import IndexLayout from "./components/index/IndexLayout";
+import MainLayout from "./components/main/MainLayout";
+
 import Login from "./components/index/Login";
 import Register from "./components/index/Register";
 import About from "./components/index/About";
@@ -13,6 +16,7 @@ function App() {
         <Route exact path="/" element={<IndexLayout component={<Login />} />} />
         <Route path="/register" element={<IndexLayout component={<Register />} />} />
         <Route path="/about" element={<IndexLayout component={<About />} />} />
+        <Route path="/profile" element={<MainLayout />} />
       </Routes>
     </BrowserRouter>
   );
