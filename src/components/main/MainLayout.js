8111abc2659786e4bba4796c3b642e528c4e82ff
flipott <ottphillipa@gmail.com>
2023-02-07 18:33:46 -0500

    Add sidebar and rightbar components

--- src/components/main/MainLayout.js
+++ src/components/main/MainLayout.js
@@ -2,6 +2,8 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import Rightbar from "./Rightbar";
 
 export default function IndexLayout(props) {
 
@@ -11,7 +13,14 @@ export default function IndexLayout(props) {
                 <Navbar firstName="Phil" />
             </div>
             <div className="main-wrap">
-                <div className="main-main">                  
+                <div className="main-sidebar">
+                    <Sidebar firstName="Phil" lastName="Ott" /> 
+                </div>
+                <div className="main-main">       
+                MAIN CONTENT HERE           
+                </div>
+                <div className="main-rightbar">
+                    <Rightbar firstName="Phil" lastName="Ott" />
                 </div>
             </div>
         </div>
---
+++ src/components/main/Sidebar.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Navbar(props) {
+    return (
+        <div className="sidebar">
+            <div className="sidebar-profile">
+                <Link to="/profile"><img src="/images/profile-temp.svg" /></Link>
+                <Link to="/profile"><p>{props.firstName} {props.lastName}</p></Link>
+            </div>
+            <ul>
+                <li>
+                    <div>
+                        <img src="/images/newspaper-dark.svg" />
+                        <Link to="/feed">Current Feed</Link>
+                    </div>
+                </li>
+                <li>
+                    <div>
+                        <img src="/images/user-dark.svg" />
+                        <Link to="/profile">Your Profile</Link>
+                    </div>
+                </li>
+                <li>
+                    <div>
+                        <img src="/images/like-dark.svg" />
+                        <Link to="/liked">Liked Posts</Link>
+                    </div>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                        <div>
+                            <img src="/images/cogs-dark.svg" />
+                            <Link to="/preferences">Preferences</Link>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            <img src="/images/door-dark.svg" />
+                            <Link to="/logout">Log Out</Link>
+                        </div>
+                    </li>
+            </ul>
+        </div> 
+    )
+}
