90f195f7deaf576fe41a9d32685ed2a8c5b0f86d
flipott <ottphillipa@gmail.com>
2023-02-07 21:36:29 -0500

    Create Preferences page component

---
+++ src/components/main/Preferences.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Preferences(props) {
+
+    const { post } = props;
+
+    return (
+        <>
+            <div className="main-top">Your Preferences</div>
+            <div className="posts-container">
+                <form action="" method="">
+                    <label htmlFor="first-name">First Name</label>
+                    <input type="text" name="first-name" minLength="2" maxLength="35" />
+
+                    <label htmlFor="last-name">Last Name</label>
+                    <input type="text" name="last-name" minLength="2" maxLength="35" />
+
+                    <button>Update Name</button>
+                </form>
+                <form action="" method="">
+                    <label htmlFor="current-password">Current Password</label>
+                    <input type="password" name="current-password" minLength="3" />
+
+                    <label htmlFor="new-password">New Password</label>
+                    <input type="password" name="new-password" minLength="3" />
+
+                    <label htmlFor="confirm-new-password">Confirm New Password</label>
+                    <input type="password" name="confirm-new-password" minLength="3" />
+
+                    <button>Update Password</button>
+                </form>
+                <form action="" method="">
+                    <button>Delete Account</button>
+                </form>
+            </div>
+        </>
+    )
+}
