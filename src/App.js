2857eab7511bb7674c705dc63a35d529454080a8
flipott <ottphillipa@gmail.com>
2023-02-12 20:04:09 -0500

    Provide proper routes and save login info as state

--- src/App.js
+++ src/App.js
@@ -28,7 +28,8 @@ const testPost = {
 function App() {
 
   const [loggedIn, setLoggedIn] = React.useState(false);
-  const [details, setDetails] = React.useState();
+  const [posts, setPosts] = React.useState();
+  const [name, setName] = React.useState();
 
   React.useEffect(() => {
       const checkLoginStatus = async () => {
@@ -36,14 +37,31 @@ function App() {
               headers: {
                   Authorization: `Bearer ${window.localStorage.getItem("token")}`
               }
-          });
+          }); 
           const json = await response.json();
           if (json["result"] === "true") {
               setLoggedIn(true);
+              getLoginItems();
+
           } else {
               setLoggedIn(false);
           }
       };
+
+      const getLoginItems = async () => {
+        const response = await fetch(`http://localhost:4001/user/${localStorage.getItem("user")}`, {
+          headers: {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+          }
+        });
+        const json = await response.json();
+        setPosts(json[0].posts);
+        setName({ "firstName": json[0].firstName, "lastName": json[0].lastName });
+      }
+
       checkLoginStatus();
   }, []);
 
@@ -53,10 +71,10 @@ function App() {
         <Route exact path="/" element={<IndexLayout component={<Login loggedIn={loggedIn} />} />} />
         <Route path="/register" element={<IndexLayout component={<Register loggedIn={loggedIn} />} />} />
         <Route path="/about" element={<IndexLayout component={<About />} />} />
-        <Route path="/profile" element={<MainLayout component={<Profile post={testPost} />} />} />
-        <Route path="/feed" element={<MainLayout component={<Feed post={testPost} loggedIn={loggedIn} />} />} />
-        <Route path="/liked" element={<MainLayout component={<Liked post={testPost} />} />} />
-        <Route path="/preferences" element={<MainLayout component={<Preferences />} />} />
+        <Route path="/profile" element={<MainLayout name={name} component={<Profile posts={posts} loggedIn={loggedIn} />} />} />
+        <Route path="/feed" element={<MainLayout name={name} component={<Feed post={testPost} loggedIn={loggedIn} />} />} />
+        <Route path="/liked" element={<MainLayout name={name} component={<Liked post={testPost} />} />} />
+        <Route path="/preferences" element={<MainLayout name={name} component={<Preferences />} />} />
       </Routes>
     </BrowserRouter>
   );
