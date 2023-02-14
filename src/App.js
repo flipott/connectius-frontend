8bf2459297c70e6c0eeb45c75a7c6324dfa560fe
flipott <ottphillipa@gmail.com>
2023-02-13 22:52:54 -0500

    Add routes for connections

--- src/App.js
+++ src/App.js
@@ -14,6 +14,10 @@ import Feed from "./components/main/Feed";
 import Liked from "./components/main/Liked";
 import Preferences from "./components/main/Preferences";
 
+import YourConnections from "./components/main/YourConnections";
+import AllConnections from "./components/main/FindConnections";
+import FindConnections from "./components/main/FindConnections";
+
 
 const testPost = {
   "firstName": "Phil",
@@ -30,6 +34,8 @@ function App() {
   const [loggedIn, setLoggedIn] = React.useState(false);
   const [posts, setPosts] = React.useState();
   const [name, setName] = React.useState();
+  const [connections, setConnections] = React.useState();
+  const [allUsers, setAllUsers] = React.useState();
 
   React.useEffect(() => {
       const checkLoginStatus = async () => {
@@ -42,6 +48,7 @@ function App() {
           if (json["result"] === "true") {
               setLoggedIn(true);
               getLoginItems();
+              getAllUsers();
 
           } else {
               setLoggedIn(false);
@@ -59,7 +66,22 @@ function App() {
         });
         const json = await response.json();
         setPosts(json[0].posts);
+        setConnections(json[0].connections);
         setName({ "firstName": json[0].firstName, "lastName": json[0].lastName });
+      }
+
+      const getAllUsers = async () => {
+        const response = await fetch(`http://localhost:4001/user/`, {
+          headers: {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        });
+        const json = await response.json();
+        console.log(json);
+        setAllUsers(json);
       }
 
       checkLoginStatus();
@@ -72,6 +94,8 @@ function App() {
         <Route path="/register" element={<IndexLayout component={<Register loggedIn={loggedIn} />} />} />
         <Route path="/about" element={<IndexLayout component={<About />} />} />
         <Route path="/profile" element={<MainLayout name={name} component={<Profile posts={posts} loggedIn={loggedIn} />} />} />
+        <Route path="/connections" element={<MainLayout name={name} component={<YourConnections connections={connections} loggedIn={loggedIn} />} />} />
+        <Route path="/find-connections" element={<MainLayout name={name} component={<FindConnections allUsers={allUsers} loggedIn={loggedIn} />} />} />
         <Route path="/feed" element={<MainLayout name={name} component={<Feed post={testPost} loggedIn={loggedIn} />} />} />
         <Route path="/liked" element={<MainLayout name={name} component={<Liked post={testPost} />} />} />
         <Route path="/preferences" element={<MainLayout name={name} component={<Preferences />} />} />
