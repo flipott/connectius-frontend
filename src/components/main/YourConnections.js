9bb32ae3d450628f8160f8a6164e12f5a0d13a79
flipott <ottphillipa@gmail.com>
2023-02-14 21:43:45 -0500

    Render connections

--- src/components/main/YourConnections.js
+++ src/components/main/YourConnections.js
@@ -4,17 +4,23 @@ import { Link } from "react-router-dom";
 export default function YourConnections(props) {
 
     const { connections } = props;
+    console.log(connections);
 
     return (
         <>
             <div className="main-top">Hello.</div>
             <div className="posts-container">
                 <div className="text-divider">Viewing Your Connections</div>
-                { connections && connections.map((connections) => {
+                { connections && connections.map((connection) => {
                     return (
-                        <div className="user-card">
-
+                        <div className="connection-card" key={connection._id}>
+                            <div className="sidebar-profile">
+                                <img src="/images/profile-temp.svg" />
+                                <p>{connection.firstName} {connection.lastName}</p>
+                            </div>
+                            <div className="border-line"></div>
                         </div>
+
                     );
                 })}                
             </div>
