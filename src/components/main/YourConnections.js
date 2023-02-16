a0126755415d3c53277dc51b32f74e30595767b1
flipott <ottphillipa@gmail.com>
2023-02-15 20:08:23 -0500

    Add form to remove connection

--- src/components/main/YourConnections.js
+++ src/components/main/YourConnections.js
@@ -4,7 +4,26 @@ import { Link } from "react-router-dom";
 export default function YourConnections(props) {
 
     const { connections } = props;
-    console.log(connections);
+    const currentUser = localStorage.getItem("user");
+
+    const disconnect = async(recipient, e) => {
+        console.log(recipient);
+        e.preventDefault()
+
+        try {
+            const response = await fetch(`http://localhost:4001/user/${recipient}/connections`, {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({currentUser})
+            });
+            const data = await response.json();
+            window.location.reload();
+        } catch(error) {
+            console.log(error);
+        }
+    }
 
     return (
         <>
@@ -19,6 +38,9 @@ export default function YourConnections(props) {
                                 <p>{connection.firstName} {connection.lastName}</p>
                             </div>
                             <div className="border-line"></div>
+                            <form onSubmit={(e) => disconnect(connection._id, e)}>
+                                <button>Disconnect</button>
+                            </form>
                         </div>
 
                     );
