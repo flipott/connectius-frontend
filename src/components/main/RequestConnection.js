ff5ff0afbf225abf874c5dc3b6fc6866d477efbc
flipott <ottphillipa@gmail.com>
2023-02-23 17:56:08 -0500

    Create RequestConnection component

---
+++ src/components/main/RequestConnection.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import ProfilePicture from "./ProfilePicture";
+
+export default function RequestConnection(props) {
+    const { request, acceptConnection, declineConnection } = props;
+    return (
+        <div className="request-card" key={request._id}>
+            <div className="sidebar-profile">
+            <Link to={`/connections/${request._id}`}> <ProfilePicture image={request.profilePicture} /></Link>
+            <p><Link to={`/connections/${request._id}`}>{request.firstName} {request.lastName}</Link></p>
+            </div>
+            <div className="border-line"></div>
+            <div className="request-buttons">
+                <form onSubmit={(e) => acceptConnection(request._id, e)}>
+                    <button>Accept</button>
+                </form>                            
+                <form onSubmit={(e) => declineConnection(request._id, e)}>
+                    <button>Decline</button>
+                </form>     
+            </div>
+        </div>
+    );
+}
