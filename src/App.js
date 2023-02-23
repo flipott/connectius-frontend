9ff12f4cdf60e649f8fa58c7750438055c3802ff
flipott <ottphillipa@gmail.com>
2023-02-22 19:36:47 -0500

    Save profile picture to state

--- src/App.js
+++ src/App.js
@@ -39,6 +39,7 @@ function App() {
   const [loggedIn, setLoggedIn] = React.useState(false);
   const [posts, setPosts] = React.useState();
   const [name, setName] = React.useState();
+  const [profilePicture, setProfilePicture] = React.useState();
   const [connections, setConnections] = React.useState();
   const [requests, setRequests] = React.useState();
   const [allUsers, setAllUsers] = React.useState();
@@ -74,7 +75,9 @@ function App() {
     setPosts(json[0].posts);
     setConnections(json[0].connections);
     setRequests(json[0].requests);
+    console.log(json[0]);
     setName({ "firstName": json[0].firstName, "lastName": json[0].lastName });
+    setProfilePicture(json[0].profilePicture);
   }
 
   const getAllUsers = async () => {
@@ -102,14 +105,14 @@ function App() {
         <Route exact path="/" element={<IndexLayout component={<Login loggedIn={loggedIn} />} />} />
         <Route path="/register" element={<IndexLayout component={<Register loggedIn={loggedIn} />} />} />
         <Route path="/about" element={<IndexLayout component={<About />} />} />
-        <Route path="/profile" element={<MainLayout name={name} component={<Profile posts={posts} loggedIn={loggedIn} />} />} />
-        <Route path="/connections" element={<MainLayout name={name} component={<YourConnections connections={connections} loggedIn={loggedIn} />} />} />
-        <Route path="/connections/:connectionId" element={<MainLayout name={name} component={<ConnectedProfile loggedIn={loggedIn} />} />} />
-        <Route path="/find-connections" element={<MainLayout name={name} component={<FindConnections allUsers={allUsers} loggedIn={loggedIn} />} />} />
-        <Route path="/requests" element={<MainLayout name={name} component={<Requests requests={requests} loggedIn={loggedIn} />} />} />
-        <Route path="/feed" element={<MainLayout name={name} component={<Feed post={testPost} loggedIn={loggedIn} />} />} />
-        <Route path="/liked" element={<MainLayout name={name} component={<Liked post={testPost} loggedIn={loggedIn} />} />} />
-        <Route path="/preferences" element={<MainLayout name={name} component={<Preferences />} />} />
+        <Route path="/profile" element={<MainLayout name={name} profilePicture={profilePicture} component={<Profile posts={posts} loggedIn={loggedIn} profilePicture={profilePicture} />} />} />
+        <Route path="/connections" element={<MainLayout name={name} profilePicture={profilePicture} component={<YourConnections connections={connections} loggedIn={loggedIn} />} />} />
+        <Route path="/connections/:connectionId" element={<MainLayout name={name} profilePicture={profilePicture} component={<ConnectedProfile loggedIn={loggedIn} />} />} />
+        <Route path="/find-connections" element={<MainLayout name={name} profilePicture={profilePicture} component={<FindConnections allUsers={allUsers} loggedIn={loggedIn} />} />} />
+        <Route path="/requests" element={<MainLayout name={name} profilePicture={profilePicture} component={<Requests requests={requests} loggedIn={loggedIn} />} />} />
+        <Route path="/feed" element={<MainLayout name={name} profilePicture={profilePicture} component={<Feed post={testPost} loggedIn={loggedIn} />} />} />
+        <Route path="/liked" element={<MainLayout name={name} profilePicture={profilePicture} component={<Liked post={testPost} loggedIn={loggedIn} />} />} />
+        <Route path="/preferences" element={<MainLayout name={name} profilePicture={profilePicture} component={<Preferences />} />} />
       </Routes>
     </BrowserRouter>
   );
