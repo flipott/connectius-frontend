8ab850f66e15bb7f9389bf84b721c4c54c72d453
flipott <ottphillipa@gmail.com>
2023-02-23 16:58:38 -0500

    Create AvailableConnection component

---
+++ src/components/main/AvailableConnection.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ProfilePicture from "./ProfilePicture";
+import { Link } from "react-router-dom";
+
+export default function AvailableConnection(props) {
+    const { user, isRequested, cancelConnectionRequest, sendConnectionRequest } = props;
+    return (
+        <div className="user-card" key={user._id}>
+            <div className="sidebar-profile">
+            <Link to={`/connections/${user._id}`}> <ProfilePicture image={user.profilePicture} /></Link>
+            <p><Link to={`/connections/${user._id}`}>{user.firstName} {user.lastName}</Link></p>
+            </div>
+            <div className="border-line"></div>
+            {
+                isRequested(user) ? 
+                <form onSubmit={(e) => cancelConnectionRequest(user._id, e)}>
+                    <button>Cancel Connection Request</button>
+                </form>                                      
+                : 
+                <form onSubmit={(e) => sendConnectionRequest(user._id, e)}>
+                    <button>Send Connection Request</button>
+                </form>                            
+            }
+        </div>
+    );
+}
