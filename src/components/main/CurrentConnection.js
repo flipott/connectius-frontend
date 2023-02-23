e03a8f06e017de5adb6da4b5c2a724ef7742655a
flipott <ottphillipa@gmail.com>
2023-02-23 17:40:31 -0500

    Create CurrentConnection component

---
+++ src/components/main/CurrentConnection.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import ProfilePicture from "./ProfilePicture";
+
+export default function CurrentConnection(props) {
+    const { connection, disconnect } = props;
+
+    return (
+        <div className="connection-card" key={connection._id}>
+            <div className="sidebar-profile">
+            <Link to={`/connections/${connection._id}`}> <ProfilePicture image={connection.profilePicture} /></Link>
+            <p><Link to={`${connection._id}`}>{connection.firstName} {connection.lastName}</Link></p>
+            </div>
+            <div className="border-line"></div>
+            <form onSubmit={(e) => disconnect(connection._id, e)}>
+                <button>Disconnect</button>
+            </form>
+        </div>
+    );
+}
