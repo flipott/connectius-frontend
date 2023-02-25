1c33bf1f63bd8b948f13aa11984023872d25d26f
flipott <ottphillipa@gmail.com>
2023-02-25 13:05:19 -0500

    Add logic for ellipses

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -10,23 +10,74 @@ export default function Pagination({postsPerPage, totalPosts, paginate, currentP
             <ul className="pagination">
                 {
                     currentPage - 1 > 0 ?
-                    <button onClick={() => paginate(currentPage-1, currentPage, postsPerPage, feedPosts)}>Prev</button>
+                    <button onClick={() => paginate(currentPage-1, currentPage, postsPerPage, feedPosts)}>&lt;</button>
                     :
-                    <button onClick={() => paginate(currentPage-1, currentPage, postsPerPage, feedPosts)} disabled={true} >Prev</button>
+                    <button onClick={() => paginate(currentPage-1, currentPage, postsPerPage, feedPosts)} disabled={true} >&lt;</button>
                 }
-                {pageNumbers.map(num => (
-                    <li key={num} className="page-item">
-                        <button onClick={() => paginate(num, currentPage, postsPerPage, feedPosts )} 
-                        className={num === currentPage ? "page-link selected" : "page-link"}>
-                            {num}
+                {pageNumbers.length > 9 ? (
+                <>
+                    {currentPage > 5 && (
+                    <li className="page-item">
+                        <button
+                        onClick={() => paginate(1, currentPage, postsPerPage, feedPosts)}
+                        className="page-link"
+                        >
+                        1
                         </button>
                     </li>
-                ))}
+                    )}
+                    {currentPage > 6 && (
+                    <li className="page-item">
+                        <span className="page-link">...</span>
+                    </li>
+                    )}
+                    {pageNumbers
+                    .filter(num => num >= currentPage - 2 && num <= currentPage + 2)
+                    .map(num => (
+                        <li key={num} className="page-item">
+                        <button
+                            onClick={() => paginate(num, currentPage, postsPerPage, feedPosts)}
+                            className={num === currentPage ? "page-link selected" : "page-link"}
+                        >
+                            {num}
+                        </button>
+                        </li>
+                    ))}
+                    {currentPage < pageNumbers.length - 5 && (
+                    <li className="page-item">
+                        <span className="page-link">...</span>
+                    </li>
+                    )}
+                    {currentPage < pageNumbers.length - 4 && (
+                    <li className="page-item">
+                        <button
+                        onClick={() =>
+                            paginate(pageNumbers.length, currentPage, postsPerPage, feedPosts)
+                        }
+                        className="page-link"
+                        >
+                        {pageNumbers.length}
+                        </button>
+                    </li>
+                    )}
+                </>
+                ) : (
+                pageNumbers.map(num => (
+                    <li key={num} className="page-item">
+                    <button
+                        onClick={() => paginate(num, currentPage, postsPerPage, feedPosts)}
+                        className={num === currentPage ? "page-link selected" : "page-link"}
+                    >
+                        {num}
+                    </button>
+                    </li>
+                ))
+)}
                 {
                     currentPage + 1 > pageNumbers.length ?
-                    <button onClick={() => paginate(currentPage + 1, currentPage, postsPerPage, feedPosts)} disabled={true}>Next</button>
+                    <button onClick={() => paginate(currentPage + 1, currentPage, postsPerPage, feedPosts)} disabled={true}>&gt;</button>
                     :
-                    <button onClick={() => paginate(currentPage + 1, currentPage, postsPerPage, feedPosts)}>Next</button>
+                    <button onClick={() => paginate(currentPage + 1, currentPage, postsPerPage, feedPosts)}>&gt;</button>
                 }            </ul>
         </>
     );
