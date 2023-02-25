eb7ca4bc19305c46cac9ce8fd200606b2323214a
flipott <ottphillipa@gmail.com>
2023-02-24 21:05:21 -0500

    Update site details

--- src/components/index/About.js
+++ src/components/index/About.js
@@ -3,12 +3,11 @@ import React from "react";
 export default function About() {
     return (
      <div className="index-about">
-        <p>Connectius is a social media website created in 2023 by Phillip Ott.</p>
+        <p>Connectius is a social media web app created in 2023 by Phillip Ott.</p>
         <p>Here's what you can do:</p>
-
         <ul>
             <li>Customize your profile page</li>
-            <li>Post text and image content</li>
+            <li>Share your thoughts</li>
             <li>Send requests to connect with other people</li>
             <li>View and interact with your connections</li>
         </ul>
