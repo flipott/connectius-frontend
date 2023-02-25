ae4f1944a2fcb647d2ff86f7f9df93ee03c7c60a
flipott <ottphillipa@gmail.com>
2023-02-25 11:36:14 -0500

    Add class name

--- src/components/main/ProfilePicture.js
+++ src/components/main/ProfilePicture.js
@@ -4,5 +4,5 @@ export default function ProfilePicture(props) {
     const { contentType, data } = props.image;
     const base64String = btoa(new Uint8Array(data.data).reduce((data, byte) => data + String.fromCharCode(byte),''));
     const imageUrl = `data:${contentType};base64,${base64String}`;
-    return <img src={imageUrl} alt="Profile Picture" />;    
+    return <img className="profile-picture" src={imageUrl} alt="Profile Picture" />;    
 }
