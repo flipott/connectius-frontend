112fd502f7549ce85e066dd5ec058bc02ec56f6b
flipott <ottphillipa@gmail.com>
2023-02-27 14:48:54 -0500

    Create banner for links

--- src/components/main/Rightbar.js
+++ src/components/main/Rightbar.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function Navbar(props) {
+export default function Rightbar(props) {
     return (
         <div className="rightbar">
-            <div className="ad">AD</div>
-            <div className="ad">AD</div>
+            <div className="ad">
+                <p>Get in touch with the creator of Connectius.</p>
+                <div>
+                    <a href="#"><img src="images/linkedin.svg" /></a>
+                    <a href="#"><img src="images/github.svg" /></a>
+                    <a href="#"><img src="images/email.svg" /></a>
+                </div>
+                <p>Copyright © 2023 Phillip Ott</p>
+            </div>
         </div>
     )
 }
