ce2c4aaf5b125c5404b3a2d67fcfb1ecb7801271
flipott <ottphillipa@gmail.com>
2023-02-28 18:35:50 -0500

    Fix error message logging

--- src/components/checkLoginStatus.js
+++ src/components/checkLoginStatus.js
@@ -1,12 +1,16 @@
 const checkLoginStatus = async () => {
-    const response = await fetch("http://localhost:4001/auth", {
-        headers: {
-            "Authorization": `Bearer ${window.localStorage.getItem("token")}`
-        }
-    }); 
-    const json = await response.json();
-    return json["result"] === "true" ? true : false;
-  };
+    try {
+        const response = await fetch("http://localhost:4001/auth", {
+            headers: {
+                "Authorization": `Bearer ${window.localStorage.getItem("token")}`
+            }
+        }); 
+        const json = await response.json();
+        return json["result"] === "true" ? true : false;
+    } catch(error) {
+        return null;
+    }
+}
 
 export default checkLoginStatus;
   
