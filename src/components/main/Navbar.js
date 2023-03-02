1ce5220f5b3e5cc1715c29087aeb24ae905a332b
flipott <ottphillipa@gmail.com>
2023-03-01 20:03:27 -0500

    Add hamburger menu

--- src/components/main/Navbar.js
+++ src/components/main/Navbar.js
@@ -1,11 +1,22 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { React } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Navbar(props) {
+    const { showMobileMenu, setShowMobileMenu } = props;
+    const navigate = useNavigate();
+
+    const logOut = (e) => {
+        e.preventDefault();
+        localStorage.removeItem("token");
+        localStorage.removeItem("user")
+        navigate("/");
+        window.location.reload();
+    }
+
     return (
         <div className="navbar">
-            <div>
-                <Link to="/feed"><img src="/images/logo-light-alternate.svg" id="main-logo" /></Link>
+            <Link to="/feed"><img src="/images/icons-light/logo-light-alternate.svg" id="main-logo" /></Link>
+            <div className="navbar-links">
                 <Link to="/feed"><p>Current Feed</p></Link>
                 <Link to="/find-connections"><p>Find Connections</p></Link>
             </div>
@@ -15,6 +26,18 @@ export default function Navbar(props) {
                     <Link to="/profile"><p>{props.firstName}</p></Link>
                 </div>
             </div>
+            <div className="navbar-mobile">
+                <a className={showMobileMenu ? "hamburger-icon show-mobile-menu" : "hamburger-icon"} onClick={() => setShowMobileMenu(prevValue => !prevValue)}><span></span></a>
+                <ul id="mobile-menu" className={showMobileMenu ? "show-mobile-menu" : ""}>
+                    <li onClick={() => setShowMobileMenu(false)}><Link to="/feed"><img src="/images/icons-light/newspaper-light.svg" />Current Feed</Link></li>
+                    <li onClick={() => setShowMobileMenu(false)}><Link to="/profile"><img src="/images/icons-light/user-light.svg" />Profile</Link></li>
+                    <li onClick={() => setShowMobileMenu(false)}><Link to="/connections"><img src="/images/icons-light/connections-light.svg" />Connections</Link></li>
+                    <li onClick={() => setShowMobileMenu(false)}><Link to="/requests"><img src="/images/icons-light/bell-light.svg" />Requests</Link></li>
+                    <li onClick={() => setShowMobileMenu(false)}><Link to="/liked"><img src="/images/icons-light/like-light.svg" />Liked Posts</Link></li>
+                    <li onClick={() => setShowMobileMenu(false)}><Link to="/preferences"><img src="/images/icons-light/cogs-light.svg" />Preferences</Link></li>
+                    <li onClick={() => setShowMobileMenu(false)}><a href="#" onClick={(e) => logOut(e)}><img src="/images/icons-light/door-light.svg" />Log Out</a></li>
+                </ul>
+            </div>
         </div>   
     )
-}
+}    
